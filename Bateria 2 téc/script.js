108fe257f0f7fe7c64297ed14feb0fe2e7c057a8
saragarcia180 <sara.garcia.souza@escola.pr.gov.br>
2025-09-16 12:18:57 -0300

    Update script.js

---
+++ Bateria 2 téc/script.js
@@ -0,0 +1,21 @@
+//Criação das constantes de som
+const sons = {
+    'q' : 'boom.wav',
+    'w' : 'clap.wav',
+    'e' : 'hihat.wav',
+    'r' : 'kick.wav',
+    't' : 'openhat.wav',
+    'y' : 'ride.wav',
+    'u' : 'snare.wav',
+    'i' : 'tink.wav',
+    'o' : 'tom.wav',
+}
+
+//Função para criar uma div para cada tecla
+const  criarDiv = (sons) => {
+    div.classlist('key');
+    div.textContent = texto;
+}
+
+exibir(sons);
+document.getElementById('container')
